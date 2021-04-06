246582952d077eb2dcc6091f0544d75a2caec986
Joonas Gaerlan <joonas2610@hotmail.com>
2021-04-06 09:27:51 +0200

    Final (updated, added, deleted files)

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,40 +1,40 @@
 /*
- * GALLERY SCRIPT
- *
- * Made for Joonas Gaerlan
- *
- * By Mads Ahlquist Jensen
- */
+* GALLERY SCRIPT
+*
+* Made for Joonas Gaerlan
+*
+* By Mads Ahlquist Jensen
+*/
 var x = document.getElementsByClassName("gallery");
 
-function setup(){
-  for (var id = 0; id < x.length; id++) {
-    move(0, id);
-  }
+function setup() {
+	for (var id = 0; id < x.length; id++) {
+		move(0, id);
+	}
 };
 
 function move(n, id) {
-  var content = x[id].children.content;
-  var length = content.childElementCount;
+	var content = x[id].children.content;
+	var length = content.childElementCount;
 
-  for (var i = 0; i < length; i++) {
-    if (content.children[i].style.display === "block") {
-      n += i;
-    }
-  }
+	for (var i = 0; i < length; i++) {
+		if (content.children[i].style.display === "block") {
+			n += i;
+		}
+	}
 
-  for (var i = 0; i < length; i++) {
-    content.children[i].style.display = "none";
-  }
+	for (var i = 0; i < length; i++) {
+		content.children[i].style.display = "none";
+	}
 
-  if (n > length-1) {
-    n = 0;
-  }
+	if (n > length - 1) {
+		n = 0;
+	}
 
-  if (n < 0) {
-    n = length-1;
-  }
+	if (n < 0) {
+		n = length - 1;
+	}
 
-  content.children[n].style.display = "block";
+	content.children[n].style.display = "block";
 
 }
--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,129 +1,131 @@
 // Portfolio Display
 const projects = {
-    all: document.getElementsByClassName("portfolio-display"),
-    work: document.getElementsByClassName("work"),
-    personal: document.getElementsByClassName("personal"),
-    sub: {
-        htmlcss: document.getElementsByClassName("htmlcss"),
-        photoshop: document.getElementsByClassName("photoshop"),
-        illustrator: document.getElementsByClassName("illustrator"),
-        indesign: document.getElementsByClassName("indesign"),
-        premierepro: document.getElementsByClassName("premierepro"),
-        aftereffects: document.getElementsByClassName("aftereffects"),
-        blender: document.getElementsByClassName("blender"),
-        other: document.getElementsByClassName("other")
-    }
+	all: document.getElementsByClassName("portfolio-display"),
+	work: document.getElementsByClassName("work"),
+	personal: document.getElementsByClassName("personal"),
+	sub: {
+		htmlcss: document.getElementsByClassName("htmlcss"),
+		photoshop: document.getElementsByClassName("photoshop"),
+		illustrator: document.getElementsByClassName("illustrator"),
+		indesign: document.getElementsByClassName("indesign"),
+		premierepro: document.getElementsByClassName("premierepro"),
+		aftereffects: document.getElementsByClassName("aftereffects"),
+		blender: document.getElementsByClassName("blender"),
+		other: document.getElementsByClassName("other")
+	}
 
 };
 
-function filterSelection () {
+function filterSelection() {
 
-    const cats = document.getElementById("portfolio-sort-btn").children
-    const subCats = document.getElementById("portfolio-filter-btn").children;
-    
-    let activeCat
-    let activeSubCats = []
+	const cats = document.getElementById("portfolio-sort-btn").children
+	const subCats = document.getElementById("portfolio-filter-btn").children;
 
-    for (let e of cats) {
-        if (e.classList.value == "active") {
-            activeCat = e.id;
-        }
-    }
+	let activeCat
+	let activeSubCats = []
 
-    for (let e of subCats) {
-        if (e.classList.value == "active") {
-            activeSubCats.push(e.id)
-        }
-    }
+	for (let e of cats) {
+		if (e.classList.value == "active") {
+			activeCat = e.id;
+		}
+	}
 
-    for (let project of projects.all[0].children) {
-        if(activeCat == "all" ? true : project.classList.value.includes(activeCat)) {
-            if(activeSubCats.length == 0) {
-                console.log(project)
-                project.style.display = "block";
-            } else if (checkForCats(project, activeSubCats)) {
-                project.style.display = "block";
-            } else {
-                project.style.display = "none";
-            }
-        } else {
-            project.style.display = "none";
-        }
-    }
+	for (let e of subCats) {
+		if (e.classList.value == "active") {
+			activeSubCats.push(e.id)
+		}
+	}
+
+	for (let project of projects.all[0].children) {
+		if (activeCat == "all" ? true : project.classList.value.includes(activeCat)) {
+			if (activeSubCats.length == 0) {
+				console.log(project)
+				project.style.display = "block";
+			} else if (checkForCats(project, activeSubCats)) {
+				project.style.display = "block";
+			} else {
+				project.style.display = "none";
+			}
+		} else {
+			project.style.display = "none";
+		}
+	}
 
 };
 
 function checkForCats(project, cats) {
-    for(let cat of cats) {
-        if(project.classList.value.includes(cat)) {
-            return true;
-        }
-    }
-    return false;
+	for (let cat of cats) {
+		if (project.classList.value.includes(cat)) {
+			return true;
+		}
+	}
+	return false;
 }
 
-function toggleCat (pressed) {
-    const elements = document.getElementById("portfolio-sort-btn").children
-    console.log(pressed)
+function toggleCat(pressed) {
+	const elements = document.getElementById("portfolio-sort-btn").children
+	console.log(pressed)
 
-    for(let e of elements) {
-        if(e.id == pressed) {
-            e.classList.value = "active";
-        } else {
-            e.classList.value = "";
-        }
-    }
-    
-    filterSelection();
+	for (let e of elements) {
+		if (e.id == pressed) {
+			e.classList.value = "active";
+		} else {
+			e.classList.value = "";
+		}
+	}
+
+	filterSelection();
 }
 
-function toggleSubCat (pressed) {
-    const e = document.getElementById(pressed);
-    console.log(pressed)
+function toggleSubCat(pressed) {
+	const e = document.getElementById(pressed);
+	console.log(pressed)
 
-    if(e.classList.value == "active") {
-        e.classList.value = ""
-    } else {
-        e.classList.value = "active"
-    }
+	if (e.classList.value == "active") {
+		e.classList.value = ""
+	} else {
+		e.classList.value = "active"
+	}
 
-    filterSelection()
+	filterSelection()
 }
 
 
-// Portfolio Maincourse Nav
+// Portfolio Maincourse Nav & Header
 
 var prevScrollPosition = window.pageYOffset;
 
 window.onscroll = function portfolioNav() {
-  let currentScrollPosition = window.pageYOffset;
-  // MOBILE
-  // Checks if device width is smaller than 611px
-  if (window.innerWidth < 611) {
-    if (prevScrollPosition > currentScrollPosition) {
-      document.getElementById("portfolio-nav").style.top = "80px";
-    } else {
-      document.getElementById("portfolio-nav").style.top = "-86px";
-    }
-  }
-  // TABLET
-  // Checks if device width is between 611px and 1024px
-  if (window.innerWidth > 610 && window.innerWidth < 1025) {
-    if (prevScrollPosition > currentScrollPosition) {
-      document.getElementById("portfolio-nav").style.top = "70px";
-    } else {
-      document.getElementById("portfolio-nav").style.top = "-32px";
-    }
-  }
-  // DESKTOP
-  // Checks if device width is bigger than 1024px
-  if (window.innerWidth > 1024) {
-    if (prevScrollPosition > currentScrollPosition) {
-      document.getElementById("portfolio-nav").style.top = "90px";
-    } else {
-      document.getElementById("portfolio-nav").style.top = "30px";
-    }
-  }
-  // Updates previous scroll position
-  prevScrollPosition = currentScrollPosition;
+	let currentScrollPosition = window.pageYOffset;
+	// MOBILE
+	// Checks if device width is smaller than 611px
+	if (window.innerWidth < 611) {
+		if (prevScrollPosition > currentScrollPosition) {
+			document.getElementById("portfolio-nav").style.top = "80px";
+			document.querySelector("header").style.top = "0px";
+		} else {
+			document.getElementById("portfolio-nav").style.top = "-166px";
+			document.querySelector("header").style.top = "-226px";
+		}
+	}
+	// TABLET
+	// Checks if device width is between 611px and 1024px
+	if (window.innerWidth > 610 && window.innerWidth < 1025) {
+		if (prevScrollPosition > currentScrollPosition) {
+			document.getElementById("portfolio-nav").style.top = "70px";
+		} else {
+			document.getElementById("portfolio-nav").style.top = "-32px";
+		}
+	}
+	// DESKTOP
+	// Checks if device width is bigger than 1024px
+	if (window.innerWidth > 1024) {
+		if (prevScrollPosition > currentScrollPosition) {
+			document.getElementById("portfolio-nav").style.top = "90px";
+		} else {
+			document.getElementById("portfolio-nav").style.top = "30px";
+		}
+	}
+	// Updates previous scroll position
+	prevScrollPosition = currentScrollPosition;
 };
