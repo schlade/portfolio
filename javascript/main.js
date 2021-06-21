fc3c6f57fc325f6e83bb7feb99da2e497381233a
schlade <joonas2610@hotmail.com>
2021-06-21 16:19:00 +0200

    Cookie Consent

--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,21 @@ function navToggle() {
 	nav.classList.toggle("menu-open");
 };
 
+// Cookie Banner
+// https://www.youtube.com/watch?v=R_-oGJBg3nw
+const cookieContainer = document.querySelector("#cookie-container");
+const cookieButton = document.querySelector("#cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
 
 // Remember Scroll Position
 var sidebar = document.querySelector("body");
